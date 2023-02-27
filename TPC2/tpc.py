a223977a582d1cf50825fc17454f4026c9f2629d
Lemos2802 <73466454+Lemos2802@users.noreply.github.com>
2023-02-27 17:56:07 +0000

    Update tpc.py

--- TPC2/tpc.py
+++ TPC2/tpc.py
@@ -1,53 +1,5 @@
 import sys
 
-#def read_input():
-#    """Função para ler o texto do canal de entrada."""
-#    return sys.stdin.read()
-#
-#def parse_input(text):
-#    """Função para extrair as sequências de dígitos do texto."""
-#    result = 0
-#    in_number = False
-#    current_number = ""
-#    for char in text:
-#        if char.isdigit():
-#            in_number = True
-#            current_number += char
-#        else:
-#            if in_number:
-#                result += int(current_number)
-#                current_number = ""
-#                in_number = False
-#            if char.lower() == 'o':
-#                if text.lower().startswith("off", pos):
-#                    break
-#            elif char.lower() == 'o':
-#                if text.lower().startswith("on", pos):
-#                    in_number = True
-#                    pos += 1
-#    return result
-#
-#def main():
-#    in_sum = True
-#    flag = True
-#    while flag:
-#        text = read_input()
-#        if not text:
-#            break
-#        if not in_sum:
-#            if "on" in text.lower():
-#                in_sum = True
-#            continue
-#        result = parse_input(text)
-#        if "=" in text:
-#            print(result)
-#            flag = False
-#        if "off" in text.lower():
-#            in_sum = False
-#
-
-#for line in sys.stdin:
-#    print(line)
 
 def reader():
      
